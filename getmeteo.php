6c11043d7f4f7be5aff7cd0ba864d86b821d99d0
Pedro Moreira <pmoreira.estg@gmail.com>
2014-10-14 12:36:15 +0100

    funcao gera tabela

--- getmeteo.php
+++ getmeteo.php
@@ -1,3 +1,36 @@
+<?php
+function forecastAsHTML($date,$desc,$icon,$tmax,$tmin) {
+	$theHTML = "";
+	$theHTML .= "<div>";
+	$theHTML .= "<table>";
+	$theHTML .= "<tr>";
+	$theHTML .= "<td colspan=\"2\">";
+	$theHTML .= $date;
+	$theHTML .= "</td>";
+	$theHTML .= "</tr>";
+	$theHTML .= "<tr>";
+	$theHTML .= "<td colspan=\"2\">";
+	$theHTML .= $desc;
+	$theHTML .= "</td>";
+	$theHTML .= "</tr>";
+	$theHTML .= "<tr>";
+	$theHTML .= "<td rowspan=\"2\">";
+	$theHTML .= "<img src=\"".$icon."\"/>";
+	$theHTML .= "</td>";
+	$theHTML .= "<td>";
+	$theHTML .= $tmax;
+	$theHTML .= "</td>";
+	$theHTML .= "</tr>";
+	$theHTML .= "<tr>";
+	$theHTML .= "<td>";
+	$theHTML .= $tmin;
+	$theHTML .= "</td>";
+	$theHTML .= "</tr>";
+	$theHTML .= "</table>";
+	$theHTML .= "</div>";
+	return $theHTML;
+}
+?>
 <html>
 	<head>
 		<meta charset="utf-8"/>
@@ -8,18 +41,25 @@
 		//api.worldweatheronline.com/free/v1/weather.ashx?q=perre%2C%20pt&format=json&num_of_days=5&key=jx6a4hxmgej238dw8x4p8vvc
 		$coreURL = "http://api.worldweatheronline.com/free/v1/weather.ashx?";
 		$sep = "&";
-		$p_q = "q=".$_GET['location'];
+		$p_q = "q=".urlencode($_GET['location']);
 		$p_nd = "num_of_days=5";
 		$p_key = "key=jx6a4hxmgej238dw8x4p8vvc";
 		$p_format = "format=json";
 		
 		$callURL = $coreURL.$p_q.$sep.$p_nd.$sep.$p_key.$sep.$p_format;
-
+		echo "<hr/>";
+		echo $callURL;
+		echo "<hr/>";
+		
 		$forecastJSON = file_get_contents($callURL);
 		
 		$forecastPHP = json_decode($forecastJSON);
 		echo "<hr/>";
 		echo $forecastPHP->data->current_condition[0]->temp_C;
+		echo "<hr/>";
+		
+		echo "<hr/>";
+		echo forecastAsHTML("14-10-2014", "sunny", "http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0001_sunny.png", "33", "12");
 		echo "<hr/>";
 		?>
 	</body>
